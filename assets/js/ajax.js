7618f5e015f764886ff1f39a8307b3cf5a943393
redfors <red-com@list.ru>
2020-04-18 17:45:31 +0200

    reload

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,21 +1,28 @@
 $(function () {
-
+let url = window.location.hostname;
     $('.ajax').on('click', function () {
         typeUser = $('#type-user').val();
-        if (typeUser == 'adventiser') {
+        if (typeUser === 'adventiser') {
             data = {'typeUser': 'playgrounds'}
         } else {
             data = {'typeUser': 'adventiser'}
         }
+
         $.ajax({
             url: '/index/',
             method: 'GET',
             data: data,
             success: function () {
                 $('.ajax').removeAttr('checked')
-                // location.reload();
-                let url = "http://redfors.ru/";
-                $(location).attr('href', url);
+                let reload;
+
+                if (url === 'localhost') {
+                    reload = "http://localhost:8000/";
+                } else {
+                    reload = "http://redfors.ru/";
+                }
+
+                $(location).attr('href', reload);
             },
             error: function () {
                 console.log('Error');
